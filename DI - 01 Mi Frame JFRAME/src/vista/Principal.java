c3d7e1301b688d21bc84fdd88415d5629620f42a
DavidRM00 <eldaviddjrodmar@gmail.com>
2020-10-09 14:45:02 +0200

    09/10/2020

--- DI - 01 Mi Frame JFRAME/src/vista/Principal.java
+++ DI - 01 Mi Frame JFRAME/src/vista/Principal.java
@@ -1,19 +1,28 @@
 package vista;
 
-import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JSplitPane;
 import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import java.awt.GridLayout;
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
+
 
 public class Principal extends JFrame {
 
-	/**
-	 * Launch the application.
-	 */
+	private JPanel contentPane;
+
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -33,6 +42,18 @@ public class Principal extends JFrame {
 	public Principal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(139, 40, 116, 23);
+		getContentPane().add(textPane);
+		
+		JTextPane textPane_1 = new JTextPane();
+		textPane_1.setBounds(139, 75, 116, 23);
+		getContentPane().add(textPane_1);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(57, 84, 48, 14);
+		getContentPane().add(lblNewLabel);
 	}
-
 }
